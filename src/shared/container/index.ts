94de9847047f78ca929276ce6385304047f5cc35
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-06-27 22:55:30 -0300

    creating tsyring singletons

---
+++ src/shared/container/index.ts
@@ -0,0 +1,12 @@
+import { container } from 'tsyringe';
+
+import { ICategoriesRepository } from '../../modules/cars/repositories/implementations/ICategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/repositories/CategoriesRepository';
+
+import { ISpecificationsRepository } from '../../modules/cars/repositories/implementations/ISpecificationsRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/SpecificationsRepository';
+
+
+container.registerSingleton<ICategoriesRepository>('CategoriesRepository', CategoriesRepository);
+container.registerSingleton<ISpecificationsRepository>('SpecificationsRepository', SpecificationsRepository);
+
