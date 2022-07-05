c1631c1c66f1e4aac843f8fc14ba3ee82bdb695c
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-04 22:43:07 -0300

    removing old avatar file

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { deleteFile } from '../../../../utils/file';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -14,8 +15,11 @@ export class UpdateUserAvatarUseCase {
     private usersRepository: IUsersRepository
   ) { }
 
-  async execute({ avatarFile, userId }: IRequest): Promise<void> {
+  async execute({ userId, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(userId);
+
+    if (user!.avatar)
+      await deleteFile(`./tmp/avatar/${user!.avatar}`);
 
     user!.avatar = avatarFile;
 
