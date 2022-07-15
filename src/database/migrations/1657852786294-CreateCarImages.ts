e4618ecdb7fd64d81c9d6fcf12cfc215d71be602
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-15 00:13:28 -0300

    Car Image migration

---
+++ src/database/migrations/1657852786294-CreateCarImages.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCarImages1657852786294 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'cars_image',
+      columns: [
+        { name: 'id', type: 'uuid' },
+        { name: 'car_id', type: 'uuid' },
+        { name: 'image_name', type: 'varchar' },
+        { name: 'created_at', type: 'timestamp', default: 'now()' }
+      ],
+      foreignKeys: [
+        {
+          name: 'FKCarImage',
+          referencedTableName: 'cars',
+          referencedColumnNames: ['id'],
+          columnNames: ['car_id'],
+          onDelete: 'cascade',
+        }
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars_image');
+  }
+}
