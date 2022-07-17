20831244804cb7427961aa4db3e1700abba7f028
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-16 22:45:41 -0300

    fixing file handler

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -2,12 +2,16 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { UploadCarImageUseCase } from './UploadCarImageUseCase';
 
+interface IFiles {
+  filename: string;
+}
 
 export class UploadCarImageController {
 
   async handle(req: Request, res: Response): Promise<Response> {
     const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
-    const { carId, imageNames } = req.body;
+    const { carId } = req.body;
+    const imageNames = (req.files as IFiles[]).map(file => file.filename);
 
     await uploadCarImageUseCase.execute(carId, imageNames);
 
--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { ensureAdmin } from '@middlewares/ensureAdmin';
 
@@ -9,9 +10,13 @@ import { UploadCarImageController } from '@modules/cars/useCases/uploadCarImage/
 
 const carsRoutes = Router();
 
+const upload = multer({
+  dest: './tmp',
+});
+
 carsRoutes.post('/', ensureAdmin, new CreateCarController().handle);
 carsRoutes.get('/', new ListCarsController().handle);
 carsRoutes.post('/specification', ensureAdmin, new CreateCarSpecificationController().handle);
-carsRoutes.post('/image', ensureAdmin, new UploadCarImageController().handle);
+carsRoutes.post('/image', ensureAdmin, upload.array('images'), new UploadCarImageController().handle);
 
 export { carsRoutes };
