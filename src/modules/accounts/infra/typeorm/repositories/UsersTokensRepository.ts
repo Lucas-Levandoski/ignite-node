abfa83b956bb6594f1096821c096f92924c3daf5
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-23 23:00:23 -0300

    Users Token Repository (refresh token)

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,25 @@
+import PostgresDataSource from '@database/index';
+import { ICreateUserTokenDTO } from '@modules/accounts/dtos/ICreateUserTokenDTO';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { Repository } from 'typeorm';
+import { UserTokens } from '../entities/UserTokens';
+
+export class UsersTokensRepository implements IUsersTokensRepository {
+
+  private repository: Repository<UserTokens>;
+
+
+  constructor() {
+    this.repository = PostgresDataSource.getRepository(UserTokens);
+  }
+
+  async create(data: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create(data);
+
+    await this.repository.save(userToken);
+
+    return userToken;
+
+  }
+
+}
